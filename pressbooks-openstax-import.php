49d461686bd7c5494c56d0a6e9728d5afb4a2c37
bdolor <bpayne@bccampus.ca>
2018-07-11 14:11:39 -0700

    update dependencies, apply coding standards

--- pressbooks-openstax-import.php
+++ pressbooks-openstax-import.php
@@ -50,28 +50,27 @@ if ( ! defined( 'POI_DIR' ) ) {
  */
 add_action(
 	'init', function () {
-		// Must meet miniumum requirements
-		if ( ! @include_once( WP_PLUGIN_DIR . '/pressbooks/compatibility.php' ) ) {
-			add_action(
-				'admin_notices', function () {
-					echo '<div id="message" class="error fade"><p>' . __( 'Openstax Import for Pressbooks cannot find a Pressbooks install.', 'pressbooks-openstax-import' ) . '</p></div>';
-				}
-			);
-
-			return;
-		} elseif ( ! version_compare( PB_PLUGIN_VERSION, '5.0.0', '>=' ) ) {
-			add_action(
-				'admin_notices', function () {
-					echo '<div id="message" class="error fade"><p>' . __( 'Openstax Import for Pressbooks requires Pressbooks 5.0.0 or greater.', 'pressbooks-openstax-import' ) . '</p></div>';
-				}
-			);
-
-			return;
-		} else {
-			require_once POI_DIR . 'autoloader.php';
+	// Must meet miniumum requirements
+	if ( ! @include_once( WP_PLUGIN_DIR . '/pressbooks/compatibility.php' ) ) {
+		add_action(
+			'admin_notices', function () {
+			echo '<div id="message" class="error fade"><p>' . __( 'Openstax Import for Pressbooks cannot find a Pressbooks install.', 'pressbooks-openstax-import' ) . '</p></div>';
 		}
+		);
+
+		return;
+	} elseif ( ! version_compare( PB_PLUGIN_VERSION, '5.0.0', '>=' ) ) {
+		add_action(
+			'admin_notices', function () {
+			echo '<div id="message" class="error fade"><p>' . __( 'Openstax Import for Pressbooks requires Pressbooks 5.0.0 or greater.', 'pressbooks-openstax-import' ) . '</p></div>';
+		}
+		);
+
+		return;
+	} else {
+		require_once POI_DIR . 'autoloader.php';
 	}
-);
+} );
 
 /**
  * Composer autoloader (if needed)
@@ -83,10 +82,11 @@ if ( file_exists( $composer ) ) {
 }
 
 /**
- * must also load the class outside of the admin_notices or network_admin_notices hooks.
- * The reason is that these hooks come after the admin_enqueue_script hook that loads the javascript.
+ * must also load the class outside of the admin_notices or
+ * network_admin_notices hooks. The reason is that these hooks come after the
+ * admin_enqueue_script hook that loads the javascript.
  */
-add_action( 'admin_init', array( 'PAnD', 'init' ) );
+add_action( 'admin_init', [ 'PAnD', 'init' ] );
 
 /**
  * Verify WP QuickLaTeX is installed and active,
@@ -94,43 +94,42 @@ add_action( 'admin_init', array( 'PAnD', 'init' ) );
  */
 add_action(
 	'admin_init', function () {
-		$path = 'wp-quicklatex/wp-quicklatex.php';
+	$path = 'wp-quicklatex/wp-quicklatex.php';
 
-		$all_plugins = get_plugins();
+	$all_plugins = get_plugins();
 
-		if ( is_plugin_active_for_network( $path ) ) {
-			// quickLaTeX plugin is installed and active, do nothing
-		} elseif ( isset( $all_plugins[ $path ] ) ) {
-			// quickLaTex is installed but not active at book level, remind the book administrator to activate it
-			if ( ! is_plugin_active( $path ) ) {
-				add_action(
-					'admin_notices', function () {
-						// don't annoy them anymore if they've dismissed the activate notice
-						if ( class_exists( 'PAnD' ) && ! PAnD::is_admin_notice_active( 'single-activate-notice-forever' ) ) {
-							return;
-						}
-						// annoy them if they haven't dismissed the activate notice
-						echo '<div data-dismissible="single-activate-notice-forever" id="message" class="notice notice-warning is-dismissible"><p>' . __( '<b>' . 'OpenStax Import: ' . '</b>' . 'Your Network Administrator has made ' . '<a target="_blank" href="https://en-ca.wordpress.org/plugins/wp-quicklatex/">' . 'WP QuickLaTeX</a>' . ' available to you from your plugins menu. WP QuickLaTeX supports multiline equations, and svg image exports. ', 'pressbooks-openstax-import' ) . '</p></div>';
-					}
-				);
-			}
-		} else {
-			// remind Network Admin to install quickLaTeX
+	if ( is_plugin_active_for_network( $path ) ) {
+		// quickLaTeX plugin is installed and active, do nothing
+	} elseif ( isset( $all_plugins[ $path ] ) ) {
+		// quickLaTex is installed but not active at book level, remind the book administrator to activate it
+		if ( ! is_plugin_active( $path ) ) {
 			add_action(
-				'network_admin_notices', function () {
-					// don't annoy them if they've dismissed install notice
-					if ( class_exists( 'PAnD' ) && ! PAnD::is_admin_notice_active( 'install-notice-forever' ) ) {
-						return;
-					}
-					// annoy them if they haven't dismissed the install notice
-					$plugin_name  = 'WP QuickLaTeX';
-					$install_link = '<a href="' . esc_url( network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=' . $plugin_name . '&TB_iframe=true&width=600&height=550' ) ) . '" target="_parent" title="More info about ' . $plugin_name . '">install</a> and activate';
-					echo '<div data-dismissible="install-notice-forever" id="message" class="notice notice-warning is-dismissible"><p>' . __( '<b>' . 'OpenStax Import:' . '</b>' . ' Please ' . $install_link . ' ' . $plugin_name . ' for multiline equations and svg image export support. ', 'pressbooks-openstax-import' ) . '</p></div>';
+				'admin_notices', function () {
+				// don't annoy them anymore if they've dismissed the activate notice
+				if ( class_exists( 'PAnD' ) && ! PAnD::is_admin_notice_active( 'single-activate-notice-forever' ) ) {
+					return;
 				}
+				// annoy them if they haven't dismissed the activate notice
+				echo '<div data-dismissible="single-activate-notice-forever" id="message" class="notice notice-warning is-dismissible"><p>' . __( '<b>' . 'OpenStax Import: ' . '</b>' . 'Your Network Administrator has made ' . '<a target="_blank" href="https://en-ca.wordpress.org/plugins/wp-quicklatex/">' . 'WP QuickLaTeX</a>' . ' available to you from your plugins menu. WP QuickLaTeX supports multiline equations, and svg image exports. ', 'pressbooks-openstax-import' ) . '</p></div>';
+			}
 			);
 		}
+	} else {
+		// remind Network Admin to install quickLaTeX
+		add_action(
+			'network_admin_notices', function () {
+			// don't annoy them if they've dismissed install notice
+			if ( class_exists( 'PAnD' ) && ! PAnD::is_admin_notice_active( 'install-notice-forever' ) ) {
+				return;
+			}
+			// annoy them if they haven't dismissed the install notice
+			$plugin_name  = 'WP QuickLaTeX';
+			$install_link = '<a href="' . esc_url( network_admin_url( 'plugin-install.php?tab=plugin-information&plugin=' . $plugin_name . '&TB_iframe=true&width=600&height=550' ) ) . '" target="_parent" title="More info about ' . $plugin_name . '">install</a> and activate';
+			echo '<div data-dismissible="install-notice-forever" id="message" class="notice notice-warning is-dismissible"><p>' . __( '<b>' . 'OpenStax Import:' . '</b>' . ' Please ' . $install_link . ' ' . $plugin_name . ' for multiline equations and svg image export support. ', 'pressbooks-openstax-import' ) . '</p></div>';
+		}
+		);
 	}
-);
+} );
 
 /*
 |--------------------------------------------------------------------------
@@ -150,13 +149,12 @@ add_action(
  */
 add_filter(
 	'pb_select_import_type', function ( $types ) {
-		if ( is_array( $types ) && ! array_key_exists( 'cnx', $types ) ) {
-			$types['zip'] = __( 'cnx.org (.zip or URL)', 'pressbooks-openstax-import' );
-		}
-
-		return $types;
+	if ( is_array( $types ) && ! array_key_exists( 'cnx', $types ) ) {
+		$types['zip'] = __( 'cnx.org (.zip or URL)', 'pressbooks-openstax-import' );
 	}
-);
+
+	return $types;
+} );
 
 /**
  * Inserts our OpenStax Class into the mix
@@ -165,11 +163,10 @@ add_filter(
  */
 add_filter(
 	'pb_initialize_import', function ( $importer ) {
-		$importer[] = new OpenStax\Cnx();
+	$importer[] = new OpenStax\Cnx();
 
-		return $importer;
-	}
-);
+	return $importer;
+} );
 
 /**
  * added for pb5 compatibility, default timeout on http->request is 5
@@ -177,11 +174,10 @@ add_filter(
  */
 add_filter(
 	'http_request_timeout', function ( $timeout ) {
-		$timeout = 5400;
+	$timeout = 5400;
 
-		return $timeout;
-	}
-);
+	return $timeout;
+} );
 
 /**
  * Pre PB v5.3.0 admins need to be able to activate the
@@ -189,52 +185,50 @@ add_filter(
  */
 add_filter(
 	'all_plugins', function ( $plugins ) {
-		$slug = 'wp-quicklatex';
+	$slug = 'wp-quicklatex';
 
-		// do nothing if it's already set
-		if ( isset( $plugins[ $slug . '/' . $slug . '.php' ] ) ) {
-			return $plugins;
-		}
+	// do nothing if it's already set
+	if ( isset( $plugins[ $slug . '/' . $slug . '.php' ] ) ) {
+		return $plugins;
+	}
 
-		if ( ! is_super_admin() ) {
-			// if it's not already active
-			if ( ! is_plugin_active_for_network( $slug . '/' . $slug . '.php' ) ) {
-				$path   = plugin_dir_path( __DIR__ );
-				$exists = file_exists( $path . '/' . $slug . '/' . $slug . '.php' );
+	if ( ! is_super_admin() ) {
+		// if it's not already active
+		if ( ! is_plugin_active_for_network( $slug . '/' . $slug . '.php' ) ) {
+			$path   = plugin_dir_path( __DIR__ );
+			$exists = file_exists( $path . '/' . $slug . '/' . $slug . '.php' );
 
-				// if file is there
-				if ( $exists ) {
-					$info                                    = get_plugin_data( $path . '/' . $slug . '/' . $slug . '.php', false, false );
-					$plugins[ $slug . '/' . $slug . '.php' ] = $info;
-				}
+			// if file is there
+			if ( $exists ) {
+				$info                                    = get_plugin_data( $path . '/' . $slug . '/' . $slug . '.php', false, false );
+				$plugins[ $slug . '/' . $slug . '.php' ] = $info;
 			}
 		}
+	}
 
-		return $plugins;
-	}, 11, 1
-);
+	return $plugins;
+}, 11, 1 );
 
 /**
  * add crude notification mechanism
  */
 add_action(
 	'admin_enqueue_scripts', function () {
-		if ( isset( $_REQUEST['page'] ) && 'pb_import' === $_REQUEST['page'] ) {
-			wp_enqueue_script( 'poi-notify', plugin_dir_url( __FILE__ ) . 'assets/scripts/notifications.js', [ 'jquery' ], null, true );
+	if ( isset( $_REQUEST['page'] ) && 'pb_import' === $_REQUEST['page'] ) {
+		wp_enqueue_script( 'poi-notify', plugin_dir_url( __FILE__ ) . 'assets/scripts/notifications.js', [ 'jquery' ], NULL, true );
 
-			$quicklatex_status       = ( is_plugin_active( 'wp-quicklatex/wp-quicklatex.php' ) ) ? 1 : 0;
-			$post_max_size_str       = ini_get( 'post_max_size' );
-			$upload_max_filesize_str = ini_get( 'upload_max_filesize' );
-			$memory_limit_str        = ini_get( 'memory_limit' );
+		$quicklatex_status       = ( is_plugin_active( 'wp-quicklatex/wp-quicklatex.php' ) ) ? 1 : 0;
+		$post_max_size_str       = ini_get( 'post_max_size' );
+		$upload_max_filesize_str = ini_get( 'upload_max_filesize' );
+		$memory_limit_str        = ini_get( 'memory_limit' );
 
-			wp_localize_script(
-				'poi-notify', 'settings', [
-					'active'     => $quicklatex_status,
-					'post_max'   => $post_max_size_str,
-					'upload_max' => $upload_max_filesize_str,
-					'memory_max' => $memory_limit_str,
-				]
-			);
-		}
+		wp_localize_script(
+			'poi-notify', 'settings', [
+				'active'     => $quicklatex_status,
+				'post_max'   => $post_max_size_str,
+				'upload_max' => $upload_max_filesize_str,
+				'memory_max' => $memory_limit_str,
+			]
+		);
 	}
-);
+} );
